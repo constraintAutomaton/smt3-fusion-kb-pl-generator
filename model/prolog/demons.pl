1e3f8ce93cc0801b3408c3b71b91667979586a7b
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2025-03-16 00:22:32 +0100

    made simple fusion work

--- model/prolog/demons.pl
+++ model/prolog/demons.pl
@@ -14,13 +14,13 @@ fuse(demon(Name1, Race1, Lv1), demon(Name2, Race2, Lv2), demon(NameR, RaceR, LvR
     demon(Name2, Race2, Lv2),
     % find the race of the resulting demon
     fuse_race(Race1, Race2, RaceR), 
-    % calculate average level
+    % calculate the level of the resulting demon
     LvAvg #= (Lv1 + Lv2) // 2,
-    % find all the candidate
     LvR #> LvAvg,
-    demon_min_level(NameR, RaceR, LvR, LvLess),
-    demon(NameR, RaceR, LvLess).
+    % check if the demon
+    demon_min_level(RaceR, LvR),
+    % find the resulting demon
+    demon(NameR, RaceR, LvR).
 
-demon_min_level(NameR, Race, Lv, LvLess) :- LvLess #< Lv, demon(NameR, Race, LvLess), demon_min_level(NameR, Race, LvLess)  .
-%demon_min_level(NameR, Race, Lv) :- demon(NameR, Race, Lv) .
+demon_min_level(RaceR, Lv):- LvMore #> Lv, demon(_, RaceR, LvMore) .
 
