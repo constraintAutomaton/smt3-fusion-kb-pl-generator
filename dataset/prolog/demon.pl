d15a44b263c1c8b080aeb34bdf47664b430a8c70
Votre Nom <Vous@exemple.com>
2025-03-29 20:59:38 +0100

    knowledge base of demon created.

---
+++ dataset/prolog/demon.pl
@@ -0,0 +1,184 @@
+demon('zouchou', kishin, 27, false).
+demon('zhen', wilder, 6, false).
+demon('yurlungur', snake, 66, false).
+demon('yatagarasu', avatar, 46, false).
+demon('yaksini', femme, 43, false).
+demon('yaka', haunt, 17, false).
+demon('wu kong', fury, 54, true).
+demon('willo\'wisp', foul, 1, false).
+demon('white rider', fiend, 52, true).
+demon('vishnu', deity, 93, false).
+demon('virtue', divine, 41, false).
+demon('valkyrie', wargod, 33, true).
+demon('uzume', megami, 18, false).
+demon('uriel', seraph, 73, true).
+demon('unicorn', holy, 21, false).
+demon('trumpeter', fiend, 77, true).
+demon('troll', fairy, 38, false).
+demon('titania', fairy, 57, false).
+demon('titan', jirae, 49, false).
+demon('throne', divine, 64, false).
+demon('thor', kishin, 76, false).
+demon('the harlot', fiend, 69, true).
+demon('taraka', femme, 20, false).
+demon('taotie', vile, 65, false).
+demon('surt', tyrant, 74, false).
+demon('sui-ki', brute, 62, false).
+demon('sudama', jirae, 13, false).
+demon('succubus', night, 37, false).
+demon('sparna', beast, 54, false).
+demon('slime', foul, 6, false).
+demon('skadi', lady, 74, true).
+demon('shiva', fury, 95, true).
+demon('shikome', femme, 32, false).
+demon('shikigami', brute, 4, false).
+demon('shiki-ouji', brute, 54, false).
+demon('shiisaa', holy, 13, false).
+demon('shadow', foul, 52, false).
+demon('setanta', fairy, 43, false).
+demon('senri', holy, 27, true).
+demon('scathach', megami, 64, false).
+demon('sati', megami, 48, false).
+demon('sarutahiko', jirae, 35, false).
+demon('sarasvati', megami, 30, false).
+demon('samael', vile, 73, true).
+demon('saki mitama', mitama, 35, false).
+demon('sakahagi', foul, 45, true).
+demon('red rider', fiend, 55, true).
+demon('raphael', seraph, 84, true).
+demon('rangda', femme, 72, false).
+demon('rakshasa', haunt, 63, false).
+demon('raja naga', snake, 37, true).
+demon('raiju', wilder, 25, false).
+demon('quetzalcoatl', snake, 55, false).
+demon('queen mab', night, 56, true).
+demon('pyro jack', fairy, 19, false).
+demon('purski', yoma, 48, false).
+demon('principality', divine, 28, false).
+demon('preta', haunt, 4, false).
+demon('power', divine, 33, false).
+demon('pixie', fairy, 2, false).
+demon('pisaca', haunt, 28, false).
+demon('phantom', foul, 42, false).
+demon('pazuzu', vile, 45, false).
+demon('parvati', lady, 57, true).
+demon('pale rider', fiend, 63, true).
+demon('ose', fallen, 45, false).
+demon('orthrus', beast, 34, false).
+demon('onkot', yoma, 37, false).
+demon('oni', brute, 25, false).
+demon('ongyo-ki', brute, 81, true).
+demon('okuninushi', kishin, 39, false).
+demon('odin', deity, 65, false).
+demon('oberon', fairy, 46, false).
+demon('nyx', night, 70, false).
+demon('nue', wilder, 31, false).
+demon('nozuchi', snake, 14, false).
+demon('nigi mitama', mitama, 29, false).
+demon('nekomata', beast, 18, false).
+demon('naga', snake, 28, false).
+demon('mou-ryo', foul, 7, false).
+demon('mothman', wilder, 43, false).
+demon('mot', tyrant, 91, false).
+demon('momunofu', brute, 20, false).
+demon('mizuchi', snake, 34, false).
+demon('mithra', deity, 78, false).
+demon('minakata', kishin, 17, false).
+demon('mikazuchi', kishin, 45, false).
+demon('michael', seraph, 90, true).
+demon('metatron', seraph, 95, true).
+demon('matador', fiend, 30, true).
+demon('makami', avatar, 22, true).
+demon('mada', vile, 83, false).
+demon('long', dragon, 44, true).
+demon('loki', tyrant, 52, false).
+demon('loa', night, 53, false).
+demon('lilith', night, 80, true).
+demon('lilim', night, 8, false).
+demon('legion', haunt, 49, false).
+demon('laksmi', megami, 54, false).
+demon('lachesis', femme, 63, false).
+demon('kusi mitama', mitama, 32, false).
+demon('kushinada', lady, 41, false).
+demon('kurama', genma, 38, true).
+demon('koumoku', kishin, 33, false).
+demon('koppa', yoma, 19, false).
+demon('kodama', jirae, 3, false).
+demon('kin-ki', brute, 59, false).
+demon('kikuri-hime', lady, 24, false).
+demon('kelpie', fairy, 26, false).
+demon('karasu', yoma, 28, true).
+demon('kali', lady, 67, false).
+demon('kaiwan', night, 47, false).
+demon('jinn', yoma, 44, false).
+demon('jikoku', kishin, 52, false).
+demon('jack frost', fairy, 7, false).
+demon('isora', yoma, 14, false).
+demon('inugami', beast, 13, false).
+demon('incubus', night, 25, false).
+demon('ikusa', brute, 44, false).
+demon('huapo', jirae, 5, false).
+demon('hresvelgr', wilder, 75, false).
+demon('horus', deity, 38, false).
+demon('high pixie', fairy, 10, true).
+demon('hell biker', fiend, 42, true).
+demon('hanuman', genma, 46, true).
+demon('gurr', raptor, 63, true).
+demon('gui xian', dragon, 24, true).
+demon('gogmagog', jirae, 55, false).
+demon('girimehkala', vile, 58, true).
+demon('garuda', avian, 63, true).
+demon('ganesha', wargod, 58, true).
+demon('gabriel', seraph, 87, true).
+demon('fuu-ki', brute, 66, false).
+demon('futomimi', kishin, 63, false).
+demon('forneus', fallen, 20, false).
+demon('fomor', night, 18, false).
+demon('flauros', fallen, 68, false).
+demon('flaemis', element, 20, false).
+demon('feng huang', holy, 36, false).
+demon('erthys', element, 7, false).
+demon('eligor', fallen, 29, false).
+demon('efreet', yoma, 52, true).
+demon('dominion', divine, 50, false).
+demon('dis', yoma, 23, false).
+demon('dionysus', fury, 44, false).
+demon('decarabia', fallen, 58, false).
+demon('datsue-ba', femme, 7, false).
+demon('dante', fiend, 80, false).
+demon('dakini', femme, 52, false).
+demon('daisoujou', fiend, 37, true).
+demon('cu chulainn', genma, 52, true).
+demon('clotho', femme, 58, false).
+demon('choronzon', haunt, 11, false).
+demon('chimera', holy, 55, false).
+demon('chatterskull', haunt, 20, false).
+demon('cerberus', beast, 61, false).
+demon('cai-zhi', avatar, 26, false).
+demon('blob', foul, 16, false).
+demon('black rider', fiend, 61, true).
+demon('black ooze', foul, 28, false).
+demon('black frost', night, 66, false).
+demon('bishamon', kishin, 72, false).
+demon('bicorn', wilder, 15, false).
+demon('berith', fallen, 37, false).
+demon('beiji-weng', fury, 61, false).
+demon('beelzebub (fly)', tyrant, 95, true).
+demon('beelzebub', tyrant, 84, false).
+demon('barong', avatar, 60, false).
+demon('baphomet', vile, 33, false).
+demon('baihu', holy, 43, false).
+demon('badb catha', beast, 23, false).
+demon('atropos', femme, 67, false).
+demon('atavaka', deity, 47, false).
+demon('archangel', divine, 18, false).
+demon('arahabaki', vile, 30, false).
+demon('ara mitama', mitama, 25, false).
+demon('aquans', element, 15, false).
+demon('apsaras', yoma, 8, false).
+demon('angel', divine, 11, false).
+demon('amaterasu', deity, 56, true).
+demon('albion', entity, 64, true).
+demon('aeros', element, 11, false).
+demon('aciel', tyrant, 77, true).
+demon('abaddon', tyrant, 69, false).
